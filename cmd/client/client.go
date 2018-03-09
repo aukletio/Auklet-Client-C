969b8948879df3bf1170fc73c251deb9dc842540
Karl Schultheisz <karl.schultheisz@esg-usa.com>
2018-03-09 16:11:55 -0500

    cmd/client: simpify main

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -10,8 +10,8 @@ import (
 	"os"
 	"strconv"
 
-	"github.com/ESG-USA/Auklet-Client/api"
-	"github.com/ESG-USA/Auklet-Client/app"
+	auklet "github.com/ESG-USA/Auklet-Client/api"
+	application "github.com/ESG-USA/Auklet-Client/app"
 	"github.com/ESG-USA/Auklet-Client/config"
 	"github.com/ESG-USA/Auklet-Client/device"
 	"github.com/ESG-USA/Auklet-Client/producer"
@@ -23,69 +23,66 @@ func usage() {
 	fmt.Printf("usage: %v command [args ...]\n", os.Args[0])
 }
 
-func main() {
+var (
+	server    proxy.Proxy
+	sock      net.Listener
+	app       *application.App
+	api       auklet.API
+	cfg       config.Config
+	prod      *producer.Producer
+	errsigged = false
+)
+
+func init() {
 	log.SetFlags(log.Lmicroseconds)
 	log.Printf("Auklet Client version %s (%s)\n", Version, BuildDate)
-	args := os.Args[1:]
+}
+
+func checkArgs() (args []string) {
+	args = os.Args[1:]
 	if len(args) == 0 {
 		usage()
 		os.Exit(1)
 	}
+	return
+}
 
-	cfg := config.FromEnv()
-	api := api.New(cfg.BaseURL, cfg.APIKey)
-	app := app.New(args, cfg.AppID)
+func checkRelease() {
 	if !api.Release(app.CheckSum) {
 		if err := app.Start(); err == nil {
 			app.Wait()
 		}
 		os.Exit(0)
 	}
+}
 
-	prod := producer.New(cfg.Brokers, api.Certificates())
+func setLogOutput() {
+	var w io.Writer
+	if cfg.Dump {
+		w = io.MultiWriter(os.Stdout, prod)
+	} else {
+		w = prod
+	}
+	log.SetOutput(w)
+}
+
+func setupProducer() {
+	prod = producer.New(cfg.Brokers, api.Certificates())
 	if prod != nil {
 		prod.LogTopic = cfg.LogTopic
 	}
-	if cfg.Dump {
-		log.SetOutput(io.MultiWriter(os.Stdout, prod))
-	} else {
-		log.SetOutput(prod)
-	}
-	go api.CreateOrGetDevice(device.MacHash, cfg.AppID)
-	sock, err := net.Listen("unixpacket", "/tmp/auklet-"+strconv.Itoa(os.Getpid()))
+}
+
+func openSocket() {
+	var err error
+	sock, err = net.Listen("unixpacket", "/tmp/auklet-"+strconv.Itoa(os.Getpid()))
 	if err != nil {
 		log.Print(err)
 	}
-	defer sock.Close()
+}
 
-	profileHandler := func(data []byte) (m producer.Message, err error) {
-		return schema.NewProfile(data, app, cfg.ProfileTopic)
-	}
-
-	errsigged := false
-	errorsigHandler := func(data []byte) (m producer.Message, err error) {
-		app.Cmd.Wait()
-		errsigged = true
-		log.Printf("app %v exited with error signal", app.Path)
-		return schema.NewErrorSig(data, app, cfg.EventTopic)
-	}
-
-	logHandler := func(data []byte) (m producer.Message, err error) {
-		m = schema.NewLog(data, cfg.LogTopic)
-		return
-	}
-
-	server := proxy.Proxy{
-		Listener: sock,
-		Producer: prod,
-		Handlers: map[string]proxy.Handler{
-			"profile": profileHandler,
-			"event":   errorsigHandler,
-			"log":     logHandler,
-		},
-	}
-
-	err = app.Start()
+func serveApp() {
+	err := app.Start()
 	if err != nil {
 		os.Exit(1)
 	}
@@ -97,4 +94,27 @@ func main() {
 			log.Print(err)
 		}
 	}
+}
+
+func main() {
+	args := checkArgs()
+	cfg = config.FromEnv()
+	api = auklet.New(cfg.BaseURL, cfg.APIKey)
+	app = application.New(args, cfg.AppID)
+
+	checkRelease()
+	setupProducer()
+	setLogOutput()
+	go api.CreateOrGetDevice(device.MacHash, cfg.AppID)
+
+	openSocket()
+	defer sock.Close()
+
+	server = proxy.Proxy{
+		Listener: sock,
+		Producer: prod,
+		Handlers: customHandlers,
+	}
+
+	serveApp()
 }
