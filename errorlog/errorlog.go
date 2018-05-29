dd973e3da99b0b6e02d5a3a0faf15fea3d077900
Karl Schultheisz <karl.schultheisz@esg-usa.com>
2018-05-29 16:17:38 -0400

    errorlog: add comments to exported functions

--- errorlog/errorlog.go
+++ errorlog/errorlog.go
@@ -9,18 +9,22 @@ import (
 
 var errorLogger = log.New(os.Stdout, "", log.Lmicroseconds|log.LstdFlags)
 
+// Print prints to the logger as in the manner of fmt.Print.
 func Print(v ...interface{}) {
 	errorLogger.Print(v...)
 }
 
+// Print prints to the logger as in the manner of fmt.Println.
 func Println(v ...interface{}) {
 	errorLogger.Println(v...)
 }
 
+// Print prints to the logger as in the manner of fmt.Printf.
 func Printf(format string, v ...interface{}) {
 	errorLogger.Printf(format, v...)
 }
 
+// SetOutput sets the output destination for the logger.
 func SetOutput(w io.Writer) {
 	errorLogger.SetOutput(w)
 }
