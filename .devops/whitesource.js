e3bea8d847e4c141b59fca67b433765c14f8afed
Ron Jenkins Jr <ronald.j.jenkins@gmail.com>
2018-06-05 14:29:51 -0400

    Stop using FSA / use the API directly

--- .devops/whitesource.js
+++ .devops/whitesource.js
@@ -1,15 +1,19 @@
 const { exec } = require('child_process');
 const https = require('follow-redirects').https;
 // Prepare variables.
-const projectToken = process.env.WHITESOURCE_PROJECT_TOKEN;
+const projectName = process.env.WHITESOURCE_PROJECT_NAME;
+const projectVersion = process.env.WHITESOURCE_PROJECT_VERSION;
 const repoDir = process.argv[2];
 var deps = [], depList = [], depsLength = 0;
 var outputJson = {
-  projectToken: projectToken,
+  coordinates: {
+    artifactId: projectName,
+    version: projectVersion
+  },
   dependencies: []
 };
 // Get all the Golang dependencies for this project.
-console.log('Converting Gopkg.lock into a WhiteSource API payload...');
+console.log('Retrieving Go dependencies and assembling WhiteSource API payload...');
 exec('dep status -json', { cwd: repoDir }, (error, stdout, stderr) => {
   if (error) {
     console.error(error);
@@ -28,78 +32,43 @@ exec('dep status -json', { cwd: repoDir }, (error, stdout, stderr) => {
     return;
   }
   deps.forEach(function(dep) {
-    // Filter out noisy/irrelevant packages.
-    var projectName = dep.ProjectRoot;
-    if (projectName === 'golang.org/x/sys') {
-      depsLength -= 1;
-      return;
-    }
+    // Get the project name.
+    var name = dep.ProjectRoot;
     // Get the project owner.
-    var owner = projectName.substring(projectName.indexOf('/') + 1);
+    var owner = name.substring(name.indexOf('/') + 1);
     owner = owner.substring(0, owner.indexOf('/'));
-    // Drop prefixes from the project name.
-    projectName = projectName.substring(projectName.lastIndexOf('/') + 1);
-    // Determine the version.
+    // Determine the revision and version.
+    var revision = dep.Revision;
     var version = dep.Version;
     if (version.indexOf('branch ') === 0) {
-      version = version.replace('branch ', '');
-      // If this is a GitHub repo, use the GitHub API to get the date of the branch using the revision.
-      if (dep.ProjectRoot.indexOf('github.com') === 0) {
-        var repo = dep.ProjectRoot.replace('github.com/', '');
-        https.get({
-          host: 'api.github.com',
-          path: `/repos/${repo}/commits/${dep.Revision}?access_token=${process.env.CHANGELOG_GITHUB_TOKEN}`,
-          headers: { 'User-Agent': 'esg-usa-bot_ci' }
-        }, function(response) {
-          var jsonResponse = '';
-          response.on('data', (chunk) => { jsonResponse += chunk; });
-          response.on('end', () => {
-            // We now have the API response. Grab the commit authored date and attach it to our project version.
-            try {
-              var date = JSON.parse(jsonResponse).commit.author.date.split('T')[0];
-              version = `${version}_${date}`;
-              // Add the dependency to our final list.
-              addDep(owner, projectName, version);
-            } catch (e) {
-              console.error(e);
-              process.exitCode = 1;
-            }
-          });
-        });
-      } else {
-        // Add the dependency to our final list.
-        addDep(owner, projectName, version);
-      }
-    } else {
-      // Add the dependency to our final list.
-      addDep(owner, projectName, version);
+      version = null;
     }
+    // Add the dependency to our final list.
+    console.log(`${name} ${revision} (Version: ${version})`);
+    // This schema comes from a request file created by FSA.
+    newDep = {
+      groupId: owner,
+      artifactId: name,
+      systemPath: `${repoDir}/Gopkg.lock`,
+      optional: false,
+      children: [],
+      exclusions: [],
+      licenses: [],
+      copyrights: [],
+      dependencyType: 'GO',
+      checksums: {},
+      commit: revision
+    };
+    if (version) newDep.version = version;
+    depList.push(newDep);
   });
+  submitToWhitesource();
 });
-
-function addDep(owner, name, version) {
-  console.log(`${owner} ${name} ${version}`);
-  depList.push({
-    filename: `${name}-${version}`,
-    name: name,
-    groupId: owner,
-    artifactId: name,
-    version: version,
-    sha1: '',
-    dependencyType: 'SOURCE_LIBRARY',
-    coordinates: `${owner}:${name}:${version}`
-  });
-  if (depList.length === depsLength) {
-    // We're finally done processing all dependencies.
-    submitToWhitesource();
-  }
-}
 
 function submitToWhitesource() {
   outputJson.dependencies = depList;
-  // Submit update to WhiteSource.
   console.log('');
-  console.log('Submitting dependencies to WhiteSource...');
+  console.log('Submitting payload to WhiteSource...');
   var req = https.request({
     method: 'POST',
     host: 'saas.whitesourcesoftware.com',
@@ -129,6 +98,7 @@ function submitToWhitesource() {
     console.error(e);
     process.exitCode = 1;
   });
-  req.write(`type=UPDATE&agent=generic&agentVersion=2.4.1&pluginVersion=1.0&token=${process.env.WHITESOURCE_ORG_TOKEN}&product=${process.env.WHITESOURCE_PRODUCT_TOKEN}&timeStamp=${Date.now()}&diff=[${JSON.stringify(outputJson)}]`);
+  // Arguments here are designed to spoof the FSA.
+  req.write(`type=UPDATE&agent=fs-agent&agentVersion=2.7.0&pluginVersion=18.5.1&token=${process.env.WHITESOURCE_ORG_TOKEN}&product=${process.env.WHITESOURCE_PRODUCT_TOKEN}&timeStamp=${Date.now()}&diff=[${JSON.stringify(outputJson)}]`);
   req.end();
 }
