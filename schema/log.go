5f86d2e4d979da7d3e7315408e750bd474486426
Karl Schultheisz <karl.schultheisz@esg-usa.com>
2018-07-23 11:38:18 -0400

    schema: convert agent logs to raw messages
    This fixes a bug in which agent logs are transported with base64
    encoding. This was happening because the JSON marshaler underlying
    the persistence layer marshals []byte to base64. By using
    json.RawMessage instead of []byte, the agent log is sent as JSON.

--- schema/log.go
+++ schema/log.go
@@ -1,10 +1,12 @@
 package schema
 
 import (
+	"encoding/json"
+
 	"github.com/ESG-USA/Auklet-Client-C/broker"
 )
 
 // NewAgentLog converts data into a broker message.
 func NewAgentLog(data []byte) (m broker.Message, err error) {
-	return broker.StdPersistor.CreateMessage(data, broker.Log)
+	return broker.StdPersistor.CreateMessage(json.RawMessage(data), broker.Log)
 }
