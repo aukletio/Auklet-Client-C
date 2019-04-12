429ff7ac35f2da39a14405d7534fc2a95ecd85fa
Karl Schultheisz <karl.schultheisz@esg-usa.com>
2019-04-12 16:10:06 -0400

    agent: break up datapoint test into multiple cases

--- agent/datapoint_test.go
+++ agent/datapoint_test.go
@@ -10,29 +10,34 @@ func TestDataPointServer(t *testing.T) {
 		data string
 	}{
 		{
-			data: `
-	{ "type":        "", "data": { "arbitrary": "json" } }
-	{ "type": "generic", "data": { "arbitrary": "json" } }
-	{
-		"type": "location",
-		"data": {
-			"speed":      1.0,
-			"longitude":  1.0,
-			"latitude":   1.0,
-			"altitude":   1.0,
-			"course":     1.0,
-			"timestamp":   10,
-			"precision":  0.1
-		}
-	}
-	{
-		"type": "motion",
-		"data": {
-			"x_axis": 1.0,
-			"y_axis": 1.0,
-			"z_axis": 1.0
-		}
-	}`,
+			data: `{ "type":        "", "data": { "arbitrary": "json" } }`,
+		},
+		{
+			data: `{ "type": "generic", "data": { "arbitrary": "json" } }`,
+		},
+		{
+			data: `{
+				"type": "location",
+				"data": {
+					"speed":      1.0,
+					"longitude":  1.0,
+					"latitude":   1.0,
+					"altitude":   1.0,
+					"course":     1.0,
+					"timestamp":   10,
+					"precision":  0.1
+				}
+			}`,
+		},
+		{
+			data: `{
+				"type": "motion",
+				"data": {
+					"x_axis": 1.0,
+					"y_axis": 1.0,
+					"z_axis": 1.0
+				}
+			}`,
 		},
 	}
 	for _, test := range tests {
