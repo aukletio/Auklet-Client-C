33de2736450c47c05fb3fa12c2c99a07ba482eb2
Karl Schultheisz <karl.schultheisz@esg-usa.com>
2019-04-16 15:40:03 -0400

    agent: remove unused Server test case field

--- agent/server_test.go
+++ agent/server_test.go
@@ -8,32 +8,16 @@ import (
 func TestServer(t *testing.T) {
 	tests := []struct {
 		input  string
-		want Message
 		problem bool
 	}{
 		{
 			input: `{"type":"message","data":"hello, world"}`,
-			want: Message{
-				Type:  "message",
-				Data:  []byte(`"hello, world"`),
-				Error: "",
-			},
 		},
 		{
 			input: `{"type":"event","data":"hello, world"}`,
-			want: Message{
-				Type:  "event",
-				Data:  []byte(`"hello, world"`),
-				Error: "",
-			},
 		},
 		{
 			input: `{"malformed`,
-			want: Message{
-				Type:  "log",
-				Data:  []byte{},
-				Error: `unexpected EOF in {"malformed`,
-			},
 			problem: true,
 		},
 	}
