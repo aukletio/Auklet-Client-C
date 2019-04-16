dfc4202c4e38043bbe3ab9a25a68edb3cddc3f21
Karl Schultheisz <karl.schultheisz@esg-usa.com>
2019-04-16 14:07:44 -0400

    agent: don't set DataPointServer.msg on error

--- agent/datapoint.go
+++ agent/datapoint.go
@@ -48,11 +48,6 @@ func (s *DataPointServer) scan() bool {
 	default:
 		buf, _ := ioutil.ReadAll(s.dec.Buffered())
 		s.err = fmt.Errorf("%v in %v", err.Error(), string(buf))
-		msg := Message{
-			Type:  "log",
-			Error: s.err.Error(),
-		}
-		s.msg = msg
 		s.dec = json.NewDecoder(s.in)
 		errorlog.Printf("DataPointServer.serve: %v in %q", err, string(buf))
 		return true
